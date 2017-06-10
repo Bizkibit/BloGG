c653d3953af5b496b901a101582b8224ba9fed6d
Farshid HM <fhmanesh@gmail.com>
2017-06-09 23:23:16 -0700

    posts controller methods and resources

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,21 @@
 class PostsController < ApplicationController
 
   def new
-    
+    @post = Post.new
   end
 
   def create
+    k = params.require(:post).permit([:title, :body])
+    @post = Post.create k
+    redirect_to post_path(@post.id)
+  end
+
+  def show
+    @post = Post.find params[:id]
+  end
+
+  def index
+    @posts = Post.all
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  
+
+  get '/', to: 'welcome#index', as: :home
+
+  resources :posts
+  # get '/posts/new', to: 'posts#new'
+  # post'/posts', to: 'posts#create', as: :posts
+  # get '/posts/:id', to: 'posts#show', as: :post
+  # get 'posts', to: 'posts#index'
+
 end
