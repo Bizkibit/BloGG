3888d08d401f231b70a3ecafdd4ce45b8e411c81
Farshid HM <fhmanesh@gmail.com>
2017-06-11 23:47:18 -0700

    implemented create method for user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,20 @@ class UsersController < ApplicationController
   end
 
   def create
-    u = params.require(:user).permit([:first_name, :last_name, :email, :password_digest])
+    u = params.require(:user).permit([:first_name, :last_name, :email, :password])
     @user = User.new u
-    
+    if @user.save
+      redirect_to home_path
+      flash[:notice] = 'new user created'
+    else
+      flash[:alert] = 'failed'
+      render :new
+    end
 
+    def edit
+      @user = User.find params[:id]
+    end
   end
+
+
 end
