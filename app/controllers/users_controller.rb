910341c39f7be2a8da69711d4010560218302520
Farshid HM <fhmanesh@gmail.com>
2017-06-11 23:35:31 -0700

    modify gemfile-user#create, new user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,11 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
+  def create
+    u = params.require(:user).permit([:first_name, :last_name, :email, :password_digest])
+    @user = User.new u
+    
+
+  end
 end
